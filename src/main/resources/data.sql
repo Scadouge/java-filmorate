4608d62f349a1d583ab0f95c0040ed94f5765ce2
Scadouge <scadouge@gmail.com>
2024-04-22 12:49:47 +0300

    refactor: использовать batch insert для вставки начальных значений в data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO genre (name) VALUES ('Комедия');
-INSERT INTO genre (name) VALUES ('Драма');
-INSERT INTO genre (name) VALUES ('Мультфильм');
-INSERT INTO genre (name) VALUES ('Триллер');
-INSERT INTO genre (name) VALUES ('Документальный');
-INSERT INTO genre (name) VALUES ('Боевик');
+INSERT INTO genre (name) VALUES ('Комедия'),
+                                ('Драма'),
+                                ('Мультфильм'),
+                                ('Триллер'),
+                                ('Документальный'),
+                                ('Боевик');
 
-INSERT INTO mpa (name, description) VALUES ('G', 'У фильма нет возрастных ограничений');
-INSERT INTO mpa (name, description) VALUES ('PG', 'детям рекомендуется смотреть фильм с родителями');
-INSERT INTO mpa (name, description) VALUES ('PG-13', 'Детям до 13 лет просмотр не желателен');
-INSERT INTO mpa (name, description) VALUES ('R', 'Лицам до 17 лет просматривать фильм можно только в присутствии взрослого');
-INSERT INTO mpa (name, description) VALUES ('NC-17', 'Лицам до 18 лет просмотр запрещён');
+INSERT INTO mpa (name, description) VALUES ('G', 'У фильма нет возрастных ограничений'),
+                                           ('PG', 'детям рекомендуется смотреть фильм с родителями'),
+                                           ('PG-13', 'Детям до 13 лет просмотр не желателен'),
+                                           ('R', 'Лицам до 17 лет просматривать фильм можно только в присутствии взрослого'),
+                                           ('NC-17', 'Лицам до 18 лет просмотр запрещён');
