1e1dcfc0af6198cdc6d83d25b94998de1f587dee
Scadouge <scadouge@gmail.com>
2024-05-04 21:51:02 +0300

    fix: импортировать классы по отдельности

--- src/main/java/ru/yandex/practicum/filmorate/dao/film/DbFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/film/DbFilmStorage.java
@@ -19,7 +19,17 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.Date;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
