0d6ecbf4d86571fc8c0268878c734e370000c3c0
helloWoor1d <147750367+helloWoor1d@users.noreply.github.com>
2024-05-07 17:53:26 +0300

    style: добавить комментарии к алгоритму рекомендаций (#29)

--- src/main/java/ru/yandex/practicum/filmorate/recommendations/SlopeOne.java
+++ src/main/java/ru/yandex/practicum/filmorate/recommendations/SlopeOne.java
@@ -24,6 +24,10 @@ public class SlopeOne {
         return getRecommendations();
     }
 
+    /*
+     * На основе входных данных строится матрица,
+     * содержащая данные о количестве раз, когда пара фильмов была просмотрена вместе.
+     */
     private void buildFreqMatrix() {
         for (Map.Entry<Long, List<Film>> e : usersFilms.entrySet()) {
             for (Film f : e.getValue()) {
@@ -42,6 +46,11 @@ public class SlopeOne {
         }
     }
 
+    /*
+     * На основе построенной матрицы составляются рекомендации.
+     * Метод анализирует, какие фильмы часто смотрят вместе с теми,
+     * что пользователь уже посмотрел, и рекомендует их.
+     */
     private Collection<Film> getRecommendations() {
         HashMap<Film, Integer> recommendations = new HashMap<>();
         for (Film f : usersFilms.get(userId)) {
